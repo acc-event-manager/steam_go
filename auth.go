07b08cac3b501929cd55570e85705c70c4fcafff
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2023-09-23 22:52:17 +0300

    Make steam_go great again

--- auth.go
+++ auth.go
@@ -1,8 +1,10 @@
 package steam_go
 
+// Modified version from github.com/solovev/steam_go
+// Credits: github.com/solovev, github.com/ramonberrutti, https://github.com/Fyb3roptik, https://github.com/anotherGoogleFan
 import (
 	"errors"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -10,124 +12,100 @@ import (
 )
 
 var (
-	steam_login = "https://steamcommunity.com/openid/login"
+	steamLogin = "https://steamcommunity.com/openid/login"
 
-	openId_mode       = "checkid_setup"
-	openId_ns         = "http://specs.openid.net/auth/2.0"
-	openId_identifier = "http://specs.openid.net/auth/2.0/identifier_select"
+	openMode       = "checkid_setup"
+	openNS         = "http://specs.openid.net/auth/2.0"
+	openIdentifier = "http://specs.openid.net/auth/2.0/identifier_select"
 
-	validation_regexp        = regexp.MustCompile("^(http|https)://steamcommunity.com/openid/id/[0-9]{15,25}$")
-	digits_extraction_regexp = regexp.MustCompile("\\D+")
+	validationRegexp       = regexp.MustCompile("^(http|https)://steamcommunity.com/openid/id/[0-9]{15,25}$")
+	digitsExtractionRegexp = regexp.MustCompile(`\D+`)
 )
 
-type OpenId struct {
+type OpenID struct {
 	root      string
 	returnUrl string
 	data      url.Values
 }
 
-func NewOpenId(r *http.Request) *OpenId {
-	id := new(OpenId)
-
+func NewOpenID(r *http.Request) *OpenID {
+	id := new(OpenID)
 	proto := "http://"
-	if r.TLS != nil {
+	if r.Header.Get("X-Forwarded-Proto") == "https" || r.TLS != nil {
 		proto = "https://"
 	}
-	id.root = proto + r.Host
-
+	if r.Header.Get("X-Forwarded-Host") == "" {
+		id.root = proto + r.Host
+	} else {
+		id.root = proto + r.Header.Get("X-Forwarded-Host")
+	}
 	uri := r.RequestURI
-	if i := strings.Index(uri, "openid"); i != -1 {
+	if i := strings.Index(uri, "openid."); i != -1 {
 		uri = uri[0 : i-1]
 	}
 	id.returnUrl = id.root + uri
-
 	switch r.Method {
 	case "POST":
+		r.ParseForm()
 		id.data = r.Form
 	case "GET":
 		id.data = r.URL.Query()
 	}
-
 	return id
 }
 
-func (id OpenId) AuthUrl() string {
-	data := map[string]string{
-		"openid.claimed_id": openId_identifier,
-		"openid.identity":   openId_identifier,
-		"openid.mode":       openId_mode,
-		"openid.ns":         openId_ns,
-		"openid.realm":      id.root,
-		"openid.return_to":  id.returnUrl,
-	}
-
-	i := 0
-	url := steam_login + "?"
-	for key, value := range data {
-		url += key + "=" + value
-		if i != len(data)-1 {
-			url += "&"
-		}
-		i++
-	}
+func (id OpenID) AuthUrl() string {
+	data := make(url.Values)
+	data.Set("openid.claimed_id", openIdentifier)
+	data.Set("openid.identity", openIdentifier)
+	data.Set("openid.mode", openMode)
+	data.Set("openid.ns", openNS)
+	data.Set("openid.realm", id.root)
+	data.Set("openid.return_to", url.QueryEscape(id.returnUrl))
+	url := steamLogin + "?" + data.Encode()
 	return url
 }
 
-func (id *OpenId) ValidateAndGetId() (string, error) {
+func (id *OpenID) ValidateAndGetID() (string, error) {
 	if id.Mode() != "id_res" {
-		return "", errors.New("Mode must equal to \"id_res\".")
+		return "", errors.New("Mode must equal to \"id_res\"")
 	}
-
 	if id.data.Get("openid.return_to") != id.returnUrl {
-		return "", errors.New("The \"return_to url\" must match the url of current request.")
+		return "", errors.New("the \"return_to url\" must match the url of current request")
 	}
-
 	params := make(url.Values)
 	params.Set("openid.assoc_handle", id.data.Get("openid.assoc_handle"))
 	params.Set("openid.signed", id.data.Get("openid.signed"))
 	params.Set("openid.sig", id.data.Get("openid.sig"))
 	params.Set("openid.ns", id.data.Get("openid.ns"))
-
 	split := strings.Split(id.data.Get("openid.signed"), ",")
 	for _, item := range split {
 		params.Set("openid."+item, id.data.Get("openid."+item))
 	}
 	params.Set("openid.mode", "check_authentication")
-
-	resp, err := http.PostForm(steam_login, params)
+	resp, err := http.PostForm(steamLogin, params)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
-	content, err := ioutil.ReadAll(resp.Body)
+	content, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
-
 	response := strings.Split(string(content), "\n")
-	if response[0] != "ns:"+openId_ns {
-		return "", errors.New("Wrong ns in the response.")
+	if response[0] != "ns:"+openNS {
+		return "", errors.New("wrong ns in the response")
 	}
 	if strings.HasSuffix(response[1], "false") {
-		return "", errors.New("Unable validate openId.")
+		return "", errors.New("unable validate openId")
 	}
-
-	openIdUrl := id.data.Get("openid.claimed_id")
-	if !validation_regexp.MatchString(openIdUrl) {
-		return "", errors.New("Invalid steam id pattern.")
+	openIDUrl := id.data.Get("openid.claimed_id")
+	if !validationRegexp.MatchString(openIDUrl) {
+		return "", errors.New("invalid steam id pattern")
 	}
-
-	return digits_extraction_regexp.ReplaceAllString(openIdUrl, ""), nil
+	return digitsExtractionRegexp.ReplaceAllString(openIDUrl, ""), nil
 }
 
-func (id OpenId) ValidateAndGetUser(apiKey string) (*PlayerSummaries, error) {
-	steamId, err := id.ValidateAndGetId()
-	if err != nil {
-		return nil, err
-	}
-	return GetPlayerSummaries(steamId, apiKey)
-}
-
-func (id OpenId) Mode() string {
+func (id OpenID) Mode() string {
 	return id.data.Get("openid.mode")
 }
