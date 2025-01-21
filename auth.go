42c672285ec7eb31057afd8a85c10ba25c07eb0e
Cyril Connan <cyril.connan@gmail.com>
2025-01-21 10:32:49 +0100

    re-add check on "return_to" param

--- auth.go
+++ auth.go
@@ -76,9 +76,9 @@ func (id *OpenID) ValidateAndGetID() (string, error) {
 	if id.Mode() != "id_res" {
 		return "", errors.New("Mode must equal to \"id_res\"")
 	}
-	// if id.data.Get("openid.return_to") != id.returnUrl {
-	// 	return "", errors.New("the \"return_to url\" must match the url of current request")
-	// }
+	if id.data.Get("openid.return_to") != id.returnUrl {
+	        return "", errors.New("the \"return_to url\" must match the url of current request")
+	}
 	params := make(url.Values)
 	params.Set("openid.assoc_handle", id.data.Get("openid.assoc_handle"))
 	params.Set("openid.signed", id.data.Get("openid.signed"))
