a83413dc0953aa0a3290cf97642065131e9f6935
Cyril Connan <cyril.connan@gmail.com>
2025-02-12 18:13:29 +0100

    update ns check

--- auth.go
+++ auth.go
@@ -102,9 +102,7 @@ func (id *OpenID) ValidateAndGetID() (string, error) {
 	}
 	response := strings.Split(string(content), "\n")
 	
-	log.Println(response[0])
 	ns := fmt.Sprintf("%s:%s", "ns", openNS)
-	log.Println(ns)
 	if response[0] != ns {
 		return "", errors.New("wrong ns in the response")
 	}
